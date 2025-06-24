f36b87b9bc29ffb16919ac5e4d1cefe760e353f3
mustafa siam <mustafasiam357@gmail.com>
2025-06-25 01:42:06 +0600

    catagory sec done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         }
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">ZaiCanvas</a>
+    <Link to={'/'} className="btn btn-ghost text-xl">ZaiCanvas</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
