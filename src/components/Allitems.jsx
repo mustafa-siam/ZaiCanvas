8c15e3bbcbb6fd2fae49552365014d93e054aaab
mustafa siam <mustafasiam357@gmail.com>
2025-06-25 11:51:03 +0600

    filter done

--- src/components/Allitems.jsx
+++ src/components/Allitems.jsx
@@ -4,9 +4,26 @@ import Craftcard from './Craftcard';
 
 const Allitems = () => {
     const craftitemsloader=useLoaderData();
-    const [craftitems,setcraftitems]=useState(craftitemsloader)
+    const [craftitems,setcraftitems]=useState(craftitemsloader);
+    const handlefilter=(select)=>{
+       if(select==='all'){
+        setcraftitems(craftitemsloader)
+       }
+       else{
+        const filterd=craftitems.filter(item=>item.customization?.toLowerCase()===select.toLowerCase())
+        setcraftitems(filterd)
+       }
+    }
     return (
         <div>
+          <div className="dropdown my-5 flex justify-end">
+  <div tabIndex={0} role="button" className="btn m-1">Filter for craft customization</div>
+  <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
+    <li><a onClick={()=>handlefilter('all')}>All</a></li>
+    <li><a onClick={()=>handlefilter('yes')}>Yes</a></li>
+    <li><a onClick={()=>handlefilter('no')}>No</a></li>
+  </ul>
+</div>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-stretch'>
             {
                 craftitems.map(craftitem=><Craftcard key={craftitem._id} craftitem={craftitem}></Craftcard>)
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
       </div>
       <ul
         tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 pr-2 shadow">
         {
           navlinks
         }
@@ -43,7 +43,7 @@ const Navbar = () => {
      {navlinks}
     </ul>
   </div>
-  <div className="navbar-end flex items-center gap-4">
+  <div className="navbar-end flex items-center md:gap-4 gap-2">
     {user ? (
       <>
         <Link to="/userprofile">
